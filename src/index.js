330157b2fac0184d9618b219282f71c355a2854f
ESAD-PC <hajkunic1@etf.unsa.ba>
2020-09-27 19:03:07 +0200

    cors whitelist changed

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(httpLogger);
 
 // main router middleware
